441259f5161d3d583edb3e012e3c99e55ef8ee43
Casper Bastholm <bastholm.casper@gmail.com>
2021-01-06 12:58:22 +0100

    Renamed ui folder to base

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from "./router.js";
 import store from "./store/index.js";
 import App from "./App.vue";
 import "./index.css";
-import BaseEmoji from "./components/ui/BaseEmoji.vue";
-import BaseButton from "./components/ui/BaseButton.vue";
+import BaseEmoji from "./components/base/BaseEmoji.vue";
+import BaseButton from "./components/base/BaseButton.vue";
 
 const app = createApp(App);
 
